2536cad3ede23a6638c785edff4066536f0c1207
fredd <freddypcarrion@gmail.com>
2022-04-18 16:37:29 -0500

    Corrected stoiip test with single variables PEX-15

--- tests/test_stoiip.py
+++ tests/test_stoiip.py
@@ -1,8 +1,8 @@
 import pytest
-from stoiip_prob import stoiip as stp
+from stoiip_prob.stoiip import stoiip
 
 
 def test_stoiip():
-    sto = stp.stoiip(250, 40, 0.20, 0.20, 1.24)
-    assert sto == 10010322.6
+    sto = stoiip(250, 40, 0.20, 0.20, 1.24)
+    assert sto == pytest.approx(10010322.6, rel=1e-6, abs=1e-12)
 
