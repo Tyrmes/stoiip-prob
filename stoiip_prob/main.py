a0fde8fbbda292679e995e2fd989ed3c8d0df41e
fredd <freddypcarrion@gmail.com>
2022-04-18 21:54:37 -0500

    Corrections done regards reviewer's suggestions PEX-13

--- stoiip_prob/main.py
+++ stoiip_prob/main.py
@@ -1,17 +1,15 @@
 import numpy as np
-# from stoiip_prob import stoiip as stp
 from stoiip_prob.stoiip import stoiip
 
 #%% Define input variables
 h = 12
 A = 450
 poro = 0.15
-swc= 0.35
+swc = 0.35
 boi = 1.1
 
 #%% Calculate STOIIP
 
-# print(stp.stoiip(A, h, poro, swc, boi))
 print(stoiip(A, h, poro, swc, boi))
 
 #%% Defined Arrays for STOIIP
@@ -22,5 +20,4 @@ poro = np.array([0.12, 0.14, 0.16])
 swc = np.array([0.30, 0.35, 0.40])
 boi = np.array([1.01, 1.1, 1.12])
 
-# print(np.around(stp.stoiip(area, h, poro, swc, boi), 2))
 print(np.around(stoiip(area, h, poro, swc, boi), 2))
