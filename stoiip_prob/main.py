9bf89af30c48bd17f5a9cb3c1204f5e75d4d6b44
cgonzalez7 <72471537+cgonzalez7@users.noreply.github.com>
2022-04-19 22:47:56 -0500

    Generation of random variables using several distributions. However, the input parameters for each distribution have to be modified if necessary, this makes reference to std, mean, c, or s parameter. PEX-14

--- stoiip_prob/main.py
+++ stoiip_prob/main.py
@@ -1,18 +1,20 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from stoiip_prob.stoiip import stoiip
+from scipy.stats import expon, lognorm, norm, triang
 
-#%% Define input variables
+# %% Define input variables
 h = 12
 A = 450
 poro = 0.15
 swc = 0.35
 boi = 1.1
 
-#%% Calculate STOIIP
+# %% Calculate STOIIP
 
 print(stoiip(A, h, poro, swc, boi))
 
-#%% Defined Arrays for STOIIP
+# %% Defined Arrays for STOIIP
 
 area = np.array([450.0, 500.0, 550.0])
 h = np.array([30.0, 40.0, 50.0])
@@ -21,3 +23,62 @@ swc = np.array([0.30, 0.35, 0.40])
 boi = np.array([1.01, 1.1, 1.12])
 
 print(np.around(stoiip(area, h, poro, swc, boi), 2))
+
+# %% Create random values for each distribution
+
+norm_values = norm.rvs(loc=0.5, scale=0.3, size=1000)
+expon_values = expon.rvs(loc=0, scale=0.3, size=1000)
+lognorm_values = lognorm.rvs(s=0.95, size=1000)
+triang_values = triang.rvs(c=0.3, loc=0, scale=1, size=1000)
+
+# %% Define values for porosity according to each distribution
+
+porosity_norm = norm_values[np.logical_and(norm_values >=0, norm_values <= 1)]
+porosity_exp = expon_values[np.logical_and(expon_values >=0, expon_values <= 1)]
+porosity_log = lognorm_values[np.logical_and(lognorm_values >=0, lognorm_values <= 1)]
+porosity_tria = triang_values[np.logical_and(triang_values >=0, triang_values <= 1)]
+
+# %%
+
+plt.hist(porosity_tria)
+plt.show()
+
+# %% Define values for saturation according to each distribution
+
+saturation_norm = norm_values[np.logical_and(norm_values >=0, norm_values <= 1)]
+saturation_exp = expon_values[np.logical_and(expon_values >=0, expon_values <= 1)]
+saturation_log = lognorm_values[np.logical_and(lognorm_values >=0, lognorm_values <= 1)]
+saturation_tria = triang_values[np.logical_and(triang_values >=0, triang_values <= 1)]
+
+# %% Define values for Boi according to each distribution
+
+boi_norm = norm_values[np.logical_and(norm_values >=1, norm_values <= 2)]
+boi_exp = expon_values[np.logical_and(expon_values >=1, expon_values <= 2)]
+boi_log = lognorm_values[np.logical_and(lognorm_values >=1, lognorm_values <= 2)]
+boi_triag = triang_values[np.logical_and(triang_values >=1, triang_values <= 2)]
+
+# %% Generating random values for thicknes and area variables
+
+norm_val2 = norm.rvs(loc=50, scale=100, size=1000)
+expon_val2 = expon.rvs(loc=50, scale=100, size=1000)
+lognorm_val2 = lognorm.rvs(s=0.95, loc=50, scale=100, size=1000)
+triang_val2 = triang.rvs(c=0.3, loc=5, scale=10, size=1000)
+
+
+# %% Define values for area according to each distribution
+
+area_norm = norm_val2[np.where(norm_val2 >=0)]
+area_exp = expon_val2[np.where(expon_val2 >=0)]
+area_log = lognorm_values[np.where(lognorm_values >=0)]
+area_triag = triang_val2[np.where(triang_val2 >=0)]
+
+# %% Define values for thickness according to each distribution
+
+thickness_norm = norm_val2[np.where(norm_val2 >=0)]
+thickness_exp = expon_val2[np.where(expon_val2 >=0)]
+thickness_log = lognorm_val2[np.where(lognorm_val2 >=0)]
+thickness_triag = triang_val2[np.where(triang_val2 >=0)]
+
+# %% Plot the distributions
+# plt.hist(area_log)
+# plt.show()
