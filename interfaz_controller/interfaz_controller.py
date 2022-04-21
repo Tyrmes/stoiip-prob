078689c2a22057698e8a14d975b0c1d9181fb031
fredd <freddypcarrion@gmail.com>
2022-04-21 08:56:57 -0500

    Button to calculate stoiip PEX-20

--- interfaz_controller/interfaz_controller.py
+++ interfaz_controller/interfaz_controller.py
@@ -1,21 +1,19 @@
 import xlwings as xw
 
 
-def main():
+def main(workbook: xw.Book = None):
     wb = xw.Book.caller()
     sheet = wb.sheets[0]
-    if sheet["A1"].value == "Hello xlwings!":
-        sheet["A1"].value = "Bye xlwings!"
-    else:
-        sheet["A1"].value = "Hello xlwings!"
-
-
-@xw.func
-def hello(name):
-    return f"Hello {name}!"
+    area = float(sheet['B4'].value)
+    h = float(sheet['B5'].value)
+    poro = float(sheet['B6'].value)
+    sw = float(sheet['B7'].value)
+    boi = float(sheet['B8'].value)
+    sheet['B10'].value = 7758 * area * h * poro * (1 - sw) / boi
 
 
 if __name__ == "__main__":
     xw.Book("interfaz_controller.xlsm").set_mock_caller()
     main()
+
 
