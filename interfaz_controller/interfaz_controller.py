96b305c827d7e1544e42946aaad70639c2671769
fredd <freddypcarrion@gmail.com>
2022-04-22 12:15:42 -0500

    Data added using numpy array PEX-20

--- interfaz_controller/interfaz_controller.py
+++ interfaz_controller/interfaz_controller.py
@@ -1,15 +1,13 @@
+import numpy as np
 import xlwings as xw
 
 
 def main(workbook: xw.Book = None):
     wb = xw.Book.caller()
     sheet = wb.sheets[0]
-    area = float(sheet['B4'].value)
-    h = float(sheet['B5'].value)
-    poro = float(sheet['B6'].value)
-    sw = float(sheet['B7'].value)
-    boi = float(sheet['B8'].value)
-    sheet['B10'].value = 7758 * area * h * poro * (1 - sw) / boi
+
+    params = sheet['B4:B8'].options(np.array, transpose=True).value
+    sheet['B10'].value = 7758 * params[0] * params[1] * params[2] * (1 - params[3]) / params[4]
 
 
 if __name__ == "__main__":
