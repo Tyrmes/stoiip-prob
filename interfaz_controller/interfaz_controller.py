93d300f3de1eba088445417d4e31996b9a5804f0
fredd <freddypcarrion@gmail.com>
2022-04-24 10:01:43 -0500

    Cell stoiip defined on the name manager from Ms Excel PEX-20

--- interfaz_controller/interfaz_controller.py
+++ interfaz_controller/interfaz_controller.py
@@ -8,11 +8,12 @@ def main(workbook: xw.Book = None):
 
     # Cells B4 to B8 are called with the name Ranges, defined on the name manager from Ms Excel
     params = sheet['Ranges'].options(np.array, transpose=True).value
-    sheet['B10'].value = 7758 * params[0] * params[1] * params[2] * (1 - params[3]) / params[4]
+    sheet['stoiip'].value = 7758 * params[0] * params[1] * params[2] * (1 - params[3]) / params[4]
 
 
 if __name__ == "__main__":
     xw.Book("interfaz_controller.xlsm").set_mock_caller()
     main()
+
 
 
