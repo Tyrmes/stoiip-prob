64f441d4241c2bdbc67a019d9cdf517bcb6a8571
fredd <freddypcarrion@gmail.com>
2022-04-27 21:36:28 -0500

    Updated task PEX-21

--- interfaz_controller/interfaz_controller.py
+++ interfaz_controller/interfaz_controller.py
@@ -4,15 +4,19 @@ import xlwings as xw
 from scipy.stats import expon, lognorm, norm, triang, uniform
 
 
-def param_stoiip(df, row, dist_col, loc_col, scale_col, sc_col, iter, lim_min_col=None, lim_max_col=None):
+def param_stoiip(df, row, dist_col, loc_col, scale_col, iter, sc_col=None, lim_min_col=None, lim_max_col=None):
 
-    if df.loc[row, dist_col] == 'Normal':
+    if df.loc[row, dist_col] == 'Log-Normal' or df.loc[row, dist_col] == 'Triangular':
+        if df.loc[row, dist_col] == 'Log-Normal':
+            param = lognorm.rvs(s=df.loc[row, sc_col], loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
+            param = np.where(param < df.loc[row, lim_min_col], df.loc[row, lim_min_col], param)
+            param = np.where(param > df.loc[row, lim_max_col], df.loc[row, lim_max_col], param)
+
+        elif df.loc[row, dist_col] == 'Triangular':
+            param = triang.rvs(c=df.loc[row, sc_col], loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
+
+    elif df.loc[row, dist_col] == 'Normal':
         param = norm.rvs(loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
-        param = np.where(param < df.loc[row, lim_min_col], df.loc[row, lim_min_col], param)
-        param = np.where(param > df.loc[row, lim_max_col], df.loc[row, lim_max_col], param)
-
-    elif df.loc[row, dist_col] == 'Log-Normal':
-        param = lognorm.rvs(s=df.loc[row, sc_col], loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
         param = np.where(param < df.loc[row, lim_min_col], df.loc[row, lim_min_col], param)
         param = np.where(param > df.loc[row, lim_max_col], df.loc[row, lim_max_col], param)
 
@@ -20,13 +24,20 @@ def param_stoiip(df, row, dist_col, loc_col, scale_col, sc_col, iter, lim_min_co
         param = expon.rvs(loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
         param = np.where(param > df.loc[row, lim_max_col], df.loc[row, lim_max_col], param)
 
-    elif df.loc[row, dist_col] == 'Triangular':
-        param = triang.rvs(c=df.loc[row, sc_col], loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
-
     elif df.loc[row, dist_col] == 'Rectangular':
         param = uniform.rvs(loc=df.loc[row, loc_col], scale=df.loc[row, scale_col], size=iter)
 
     return param
+
+# Name of columns
+
+
+dist = 'Distribución'
+loc = 'Loc'
+scale = 'Scale'
+sc = 'Sc'
+Lim_min = 'Límite min'
+Lim_max = 'Límite max'
 
 
 def main(workbook: xw.Book = None):
@@ -42,13 +53,20 @@ def main(workbook: xw.Book = None):
     df_stoiip = sheet['df_stoiip'].options(pd.DataFrame, index=False, expand='table').value
     iterations = sheet['iterations'].value
 
-    area = param_stoiip(df_stoiip, 0, 'Distribución', 'Loc', 'Scale', 'Sc', iterations, 'Límite min', 'Límite max')
-    thickness = param_stoiip(df_stoiip, 1, 'Distribución', 'Loc', 'Scale', 'Sc', iterations, 'Límite min', 'Límite max')
-    porosity = param_stoiip(df_stoiip, 2, 'Distribución', 'Loc', 'Scale', 'Sc', iterations, 'Límite min', 'Límite max')
-    swc = param_stoiip(df_stoiip, 3, 'Distribución', 'Loc', 'Scale', 'Sc', iterations, 'Límite min', 'Límite max')
-    boi = param_stoiip(df_stoiip, 4, 'Distribución', 'Loc', 'Scale', 'Sc', iterations, 'Límite min', 'Límite max')
+    area = param_stoiip(df_stoiip, 0, dist, loc, scale, iterations, sc, Lim_min, Lim_max)
+    thickness = param_stoiip(df_stoiip, 1, dist, loc, scale, iterations, sc, Lim_min, Lim_max)
+    porosity = param_stoiip(df_stoiip, 2, dist, loc, scale, iterations, sc, Lim_min, Lim_max)
+    swc = param_stoiip(df_stoiip, 3, dist, loc, scale, iterations, sc, Lim_min, Lim_max)
+    boi = param_stoiip(df_stoiip, 4, dist, loc, scale, iterations, sc, Lim_min, Lim_max)
 
     sheet['stoiip_prob'].value = (7758 * area * thickness * porosity * (1 - swc) / boi).mean()
+    sheet['Std_stoiip'].value = (7758 * area * thickness * porosity * (1 - swc) / boi).std()
+    sheet['p10_stoiip'].value = np.percentile((7758 * area * thickness * porosity * (1 - swc) / boi), 10)
+    sheet['p50_stoiip'].value = np.percentile((7758 * area * thickness * porosity * (1 - swc) / boi), 50)
+    sheet['p90_stoiip'].value = np.percentile((7758 * area * thickness * porosity * (1 - swc) / boi), 90)
+
+    sheet_1 = wb.sheets[1]
+    sheet_1['stoiip_array'].value = 7758 * area * thickness * porosity * (1 - swc) / boi
 
 
 if __name__ == "__main__":
